5db3a13abb73cc08292bcece6b3ac9e62bcd59f4
Rayyan Khan <weatherwhiz06@gmail.com>
2024-02-02 09:07:36 -0500

    Hello World!

---
+++ app.js
@@ -0,0 +1,11 @@
+const amount = 9
+
+if (amount < 10) {
+    console.log('small number')
+} else {
+    console.log('large number')
+}
+
+
+// We can also use template strings (``)
+console.log(`Hello, world! It's my first Node App!`)
