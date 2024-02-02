35fc161dc20257b4d9d541e97e1e9ce731b7ff2c
Rayyan Khan <weatherwhiz06@gmail.com>
2024-02-02 09:48:09 -0500

    4-5 Modules

--- 1-intro.js
+++ 1-intro.js
@@ -1,11 +1,11 @@
 const amount = 9
 
 if (amount < 10) {
-    console.log('small number');
+    console.log('small number')
 } else {
-    console.log('large number');
+    console.log('large number')
 }
 
 
 // We can also use template strings (``)
-console.log(`Hello, world! It's my first Node App!`);;
+console.log(`Hello, world! It's my first Node App!`)
--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-// GLOBALS - NO WINDOW!!
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
 
-// __dirname  - path to current directory
-// __filename - file name
-// require    - function to use modules (CommonJS)
-// module     - info about current module (file)
-// process    - info about env where the program is being executed
-
-console.log(__dirname);
-setInterval(()=>{
-    console.log('Hello world')
-}, 1000)
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
