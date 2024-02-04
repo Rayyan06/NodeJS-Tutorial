bb0ee7dac02b4f7bf7bf97c3dd8363c72ead059b
Rayyan Khan <weatherwhiz06@gmail.com>
2024-02-04 09:41:52 -0500

    FS Module

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const path = require('path')
+const { readFile, writeFile } = require('fs');
 
-console.log(path.sep);
-
-const filePath = path.join('/content/', 'subfolder', 'test.txt')
-console.log(filePath);
-
-const base = path.basename(filePath)
-console.log(base);
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
-console.log(absolute);
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if(err) {
+        console.log(err);
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if(err) {
+            console.log(err);
+            return
+        }
+        const second = result;
+        writeFile(
+            './content/result-async.txt', 
+            `Here is the result ${first}, ${second}`,
+            (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+            }
+        )
+    })
+})
